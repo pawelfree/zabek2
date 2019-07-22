b2133dce6a8a6bba6b817cd1752733fe9b7298fc
Paweł Dudek <pawelfree@wp.pl>
2019-07-22 18:08:58 +0200

    config czesc 3

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,14 +6,19 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-import { RolesGuard } from './app/auth/roles.guard';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+  );
+
   const globalPrefix = 'api';
   const port = process.env.port || 3333;
 
   app.setGlobalPrefix(globalPrefix);
+  app.useStaticAssets(join(__dirname, '..', './dist/apps/frontend'));
   app.enableCors();
 
   await app.listen(port, () => {
