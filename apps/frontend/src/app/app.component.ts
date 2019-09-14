410acb03add73ed222f5be47f2d9268e13ae5c54
Pawel Free <pawelfree@wp.pl>
2019-09-14 23:59:40 +0200

    przyda sie do ssr

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from './store/app.reducer';
 import * as AuthActions from './auth/store/auth.actions';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'zabek-root',
@@ -10,9 +11,13 @@ import * as AuthActions from './auth/store/auth.actions';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private readonly store: Store<AppState>) {}
+  constructor(
+    private readonly store: Store<AppState>,
+    @Inject(PLATFORM_ID) private platformId) {}
 
   ngOnInit() {
-    this.store.dispatch(AuthActions.autoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(AuthActions.autoLogin());
+    }
   }
 }
