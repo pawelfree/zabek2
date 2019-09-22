685291bc2ebf514df739c471b5980ab346d51592
Maciej Skorzynski <maciej.skorzynski@gmail.com>
2019-09-22 13:17:45 +0200

    wip exam create: pozostałe pola dodane

--- apps/frontend/src/app/user-dashboard/exam-create/exam-create.component.ts
+++ apps/frontend/src/app/user-dashboard/exam-create/exam-create.component.ts
@@ -28,11 +28,10 @@ export class ExamCreateComponent implements OnInit {
       examinationDate: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(10), Validators.maxLength(10)] // chyba potrzebny jest validator na date w formacie polskim
       }),
-//      examinationType: new FormControl(null, {
-//        validators: [Validators.required] // Czy potrzebny jest customwoy validator, ktory sprawdzi czy wartosc jest elementem ze slownika?
-//     }),      
-      examinationFile: new FormControl(null, { // TODO: jak obsluzyc validator dla pliku badania?
-        validators: [Validators.nullValidator] 
+      examinationType: new FormControl(null, {
+        validators: [Validators.required] // Czy potrzebny jest customwoy validator, ktory sprawdzi czy wartosc jest elementem ze slownika?
+     }),      
+      examinationFile: new FormControl(null, { 
       }),
       patientFullName: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
@@ -42,6 +41,18 @@ export class ExamCreateComponent implements OnInit {
       }),
       patientAge: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(1), Validators.maxLength(3)]
+      }),
+      patientAck: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      doctorFullName: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
+      }),
+      doctorQualificationsNo: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(7), Validators.maxLength(7)]
+      }),
+      sendEmailTo: new FormControl(null, {
+        validators: [Validators.required, Validators.email] 
       })
     });
 
@@ -53,7 +64,6 @@ export class ExamCreateComponent implements OnInit {
         this.examService.getExam(this._id).subscribe(examData => {
           this.isLoading = false;
           this.form.setValue({
-            //TODO uzupełnić pozostałe pola z badania
             examinationDate:  examData.examinationDate,
             examinationType:  examData.examinationType,
             examinationFile:  examData.examinationFile,
@@ -72,7 +82,7 @@ export class ExamCreateComponent implements OnInit {
       }
     });
   }
-// TODO dodac pozostałe pola
+
   onSaveExam() {
     if (this.form.invalid) {
       return;
