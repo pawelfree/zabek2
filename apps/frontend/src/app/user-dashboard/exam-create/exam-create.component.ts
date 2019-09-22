73f353b79fed70fd13da9b6c26c1caaa200db92f
Maciej Skorzynski <maciej.skorzynski@gmail.com>
2019-09-22 13:26:17 +0200

    wip exam create

--- apps/frontend/src/app/user-dashboard/exam-create/exam-create.component.ts
+++ apps/frontend/src/app/user-dashboard/exam-create/exam-create.component.ts
@@ -46,13 +46,13 @@ export class ExamCreateComponent implements OnInit {
         validators: [Validators.required]
       }),
       doctorFullName: new FormControl(null, {
-        validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
+        validators: [Validators.required, Validators.minLength(3), Validators.maxLength(50)] //to pole powinno być z listy wyboru, najlepiej typu autocomplete
       }),
       doctorQualificationsNo: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(7), Validators.maxLength(7)]
       }),
       sendEmailTo: new FormControl(null, {
-        validators: [Validators.required, Validators.email] 
+        validators: [Validators.required, Validators.email] // ten email powinien się podpowiadac z profilu lekarza, ale user moze go zmienic
       })
     });
 
