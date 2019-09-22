bb3db1593ef83bc37b131caf3adb0fe3ebe0c139
Pawel Free <pawelfree@wp.pl>
2019-09-22 19:25:05 +0200

    zabawy z data

--- apps/frontend/src/app/user-dashboard/exam-create/exam-create.component.ts
+++ apps/frontend/src/app/user-dashboard/exam-create/exam-create.component.ts
@@ -4,20 +4,28 @@ import { ExamService } from '../../_services';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { PeselValidator, CustomValidator } from '../../_validators';
 
-import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
-import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 
 @Component({
   selector: 'zabek-exam-create',
   templateUrl: './exam-create.component.html',
   styleUrls: ['./exam-create.component.css'],
   providers: [
-    {provide: MAT_DATE_LOCALE, useValue: 'pl-PL'},
-
-    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
-    // `MatMomentDateModule` in your applications root module.
+    {provide: MAT_DATE_LOCALE, useValue: 'pl'},
     {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+    {provide: MAT_DATE_FORMATS, useValue: {
+                  parse: {
+                      dateInput: "DD-MM-YYYY"
+                  },
+                  display: {
+                      dateInput: "DD-MM-YYYY",
+                      monthYearLabel: "MMMM YYYY",
+                      dateA11yLabel: "LL",
+                      monthYearA11yLabel: "MMMM YYYY"
+                  }
+                }
+    }
   ],
 })
 export class ExamCreateComponent implements OnInit {
