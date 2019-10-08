e259f50721c4d21b3d0bd7e3646e92ccba6af5c9
Maciej Skorzynski <maciej.skorzynski@gmail.com>
2019-10-08 10:56:13 +0200

    wip: lista badan dla lekarza

---
+++ apps/frontend/src/app/doctor-dashboard/_datasource/exam-list.datasource.ts
@@ -0,0 +1,48 @@
+import { CollectionViewer, DataSource } from '@angular/cdk/collections';
+import { Observable, BehaviorSubject, of } from 'rxjs';
+import { ExamService } from '../../_services';
+import { Examination } from '../../_models';
+import { catchError, finalize, map , tap} from 'rxjs/operators';
+
+export class ExamListDataSource extends DataSource<Examination> {
+
+  private examsSubject = new BehaviorSubject<Examination[]>([]);
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+
+  public dataCount = 0; 
+  public itemsOnPage = 0;
+  public loading$ = this.loadingSubject.asObservable();
+
+  constructor (
+    private readonly examService: ExamService, 
+    private readonly pageSize
+  ) {
+    super();
+  }
+
+  connect(collectionViewer: CollectionViewer): Observable<Examination[] | readonly Examination[]> {
+    return this.examsSubject.asObservable();
+  }  
+  
+  disconnect(collectionViewer: CollectionViewer): void {
+    this.examsSubject.complete();
+    this.loadingSubject.complete();
+  }
+
+  loadExams(pageIndex = 0, pageSize = this.pageSize) {
+    this.loadingSubject.next(true);
+    this.examsSubject.subscribe();
+
+    this.examService.getExams(pageSize, pageIndex) 
+      .pipe(
+        catchError(() => of<{exams: Examination[], count: number}>({exams: [], count: 0})),
+        finalize(() => this.loadingSubject.next(false))
+      )
+      .subscribe(({exams, count}) => {
+        this.itemsOnPage = exams.length;
+        this.dataCount = count;
+        this.examsSubject.next([...exams])
+      });
+  }
+  
+}
---
+++ apps/frontend/src/app/doctor-dashboard/exam-list/exam-list.component.ts
@@ -0,0 +1,69 @@
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ViewChild,
+  AfterViewInit
+} from '@angular/core';
+import { MatPaginator, MatDialog } from '@angular/material';
+import { ExamListDataSource } from '../_datasource/exam-list.datasource';
+import { ExamService } from '../../_services';
+import { tap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { ConfirmationComponent } from '../../common-dialogs/confirmation/confirmation.component';
+
+@Component({
+  selector: 'zabek-exam-list',
+  templateUrl: './exam-list.component.html',
+  styleUrls: ['./exam-list.component.css']
+})
+export class ExamListComponent implements AfterViewInit, OnInit, OnDestroy {
+  examsPerPage = 10;
+  currentPage = 0;
+  title = 'angular-confirmation-dialog';
+  // order of columns on the view
+  displayedColumns = [
+    'examinationDate',
+    'examinationType',
+    'patientFullName',
+    'patientPesel',
+    'patientIsFemale',
+    'patientAge',   
+    'examinationFile',
+    'actions'
+  ];
+  dataSource: ExamListDataSource;
+  paginatorSub: Subscription = null;
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  constructor(private readonly examService: ExamService, public dialog: MatDialog) {}
+
+  ngOnInit() {
+    this.dataSource = new ExamListDataSource(
+      this.examService,
+      this.examsPerPage
+    );
+    this.dataSource.loadExams(this.currentPage, this.examsPerPage);
+  }
+
+  ngAfterViewInit() {
+    this.paginatorSub = this.paginator.page
+      .pipe(tap(() => this.loadExamsPage()))
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    if (this.paginatorSub) {
+      this.paginatorSub.unsubscribe();
+      this.paginatorSub = null;
+    }
+  }
+
+  loadExamsPage() {
+    this.dataSource.loadExams(
+      this.paginator.pageIndex,
+      this.paginator.pageSize
+    );
+  }
+}
