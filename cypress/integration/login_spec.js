2967e5b4b31cc3f733777025837391650969d065
Maciej Skorzynski <maciej.skorzynski@gmail.com>
2020-02-02 10:36:00 +0100

    cypress first test - login sadmin user

---
+++ cypress/integration/login_spec.js
@@ -0,0 +1,21 @@
+describe('Login test', function () {
+    it('Otworz strone logowania', function () {
+        cy.visit('/');
+    })
+
+    it('Logowanie sadmin-a', function () {
+        // destructuring assignment of the this.currentUser object
+        const username = 'sadmin@zabek.pl';
+        const password = 'sadmin';
+    
+        cy.visit('/login')
+    
+        cy.get('input[name=username]').type(username)
+        cy.get('input[name=password]').type(`${password}{enter}`)
+
+        cy.url().should('include', '/admin/user/list')
+    
+        // w prawym górnym rogu powinien być login usera
+        cy.get('a[name=loggedUser]').should('contain', username)
+      })
+})
